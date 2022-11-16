e2401e8104b8ea0afcf3d8fa81ccefb924152c39
Minu Song <smw0807@gmail.com>
2022-11-16 16:04:39 +0900

    다크모드 적용 테스트.... 일단 안됨

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HeaderMenu from '@/components/header/HeaderMenu.vue';
-import SideMenu from '@/components/side/SideMenu.vue';
+  // This starter template is using Vue 3 <script setup> SFCs
+  // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+  import HeaderMenu from '@/components/header/HeaderMenu.vue';
+  import SideMenu from '@/components/side/SideMenu.vue';
 </script>
 
 <template>
@@ -24,15 +24,15 @@ import SideMenu from '@/components/side/SideMenu.vue';
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.vue:hover {
+    filter: drop-shadow(0 0 2em #42b883aa);
+  }
 </style>
--- src/plugins/element-plus.ts
+++ src/plugins/element-plus.ts
@@ -6,7 +6,8 @@
 import { App } from 'vue';
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
-import 'element-plus/theme-chalk/display.css';
+// import 'element-plus/theme-chalk/display.css';
+import 'element-plus/theme-chalk/dark/css-vars.css';
 
 export const setupElementPlus = (app: App) => {
   app.use(ElementPlus);
