50e687e5169364abd1675d530d9743a5b0b3334f
Minu Song <smw0807@gmail.com>
2022-11-18 18:07:51 +0900

    1, 3인칭 랭크 데이터 표기가능하게 작업 완료

--- src/views/search/profile.vue
+++ src/views/search/profile.vue
@@ -12,6 +12,7 @@
   import type {
     ISearchForm,
     IGameRankStats,
+    IGameStats,
     IPlayerSeasonRank,
   } from '@/interfaces';
 
@@ -32,8 +33,20 @@
   // 1인칭 모드 유무
   const hasFPP: boolean = params.platform === 'kakao' ? false : true;
 
-  // data
+  // 3인칭 랭크 데이터
+  let tppRankSolo: Ref<IGameRankStats | object> = ref({});
   let tppRankSquad: Ref<IGameRankStats | object> = ref({});
+  // 1인칭 랭크 데이터
+  let fppRankSolo: Ref<IGameRankStats | object> = ref({});
+  let fppRankSquad: Ref<IGameRankStats | object> = ref({});
+  // 3인칭 일반 데이터
+  let tppSolo: Ref<IGameStats | object> = ref({});
+  let tppDuo: Ref<IGameStats | object> = ref({});
+  let tppSquad: Ref<IGameStats | object> = ref({});
+  // 1인칭 일반 데이터
+  let fppSolo: Ref<IGameStats | object> = ref({});
+  let fppDuo: Ref<IGameStats | object> = ref({});
+  let fppSquad: Ref<IGameStats | object> = ref({});
 
   // 플랫폼 시즌 정보 세팅
   const setSeason = async () => {
@@ -45,25 +58,73 @@
   };
 
   // 스탯 데이터
-  const createdStat = (mode: string): IGameRankStats | object => {
+  const createdRankStat = (mode: string): IGameRankStats | object => {
     let result: IGameRankStats | object = {};
-    if (mode === 'solo') {
-      result = store.rank.data?.attributes.rankedGameModeStats.solo;
-    } else if (mode === 'squad') {
-      result = store.rank.data?.attributes.rankedGameModeStats.squad;
-    } else {
-      alert('Need Game Mode Property\nsolo or duo or squad');
-      result = {};
+    switch (mode) {
+      case 'solo':
+        result = store.rank.data?.attributes.rankedGameModeStats.solo;
+        break;
+      case 'solo-fpp':
+        result = store.rank.data?.attributes.rankedGameModeStats['solo-fpp'];
+        break;
+      case 'squad':
+        result = store.rank.data?.attributes.rankedGameModeStats.squad;
+        break;
+      case 'squad-fpp':
+        result = store.rank.data?.attributes.rankedGameModeStats['squad-fpp'];
+        break;
+      default:
+        result = {};
+        break;
     }
-    console.log('createdStat : ', result);
+    return result;
+  };
+  const createdStat = (mode: string): IGameStats | object => {
+    let result: IGameStats | object = {};
+    switch (mode) {
+      case 'solo':
+        result = store.normal.data?.attributes.gameModeStats.solo;
+        break;
+      case 'solo-fpp':
+        result = store.normal.data?.attributes.gameModeStats['solo-fpp'];
+        break;
+      case 'duo':
+        result = store.normal.data?.attributes.gameModeStats.duo;
+        break;
+      case 'duo-fpp':
+        result = store.normal.data?.attributes.gameModeStats['duo-fpp'];
+        break;
+      case 'squad':
+        result = store.normal.data?.attributes.gameModeStats.squad;
+        break;
+      case 'squad-fpp':
+        result = store.normal.data?.attributes.gameModeStats['squad-fpp'];
+        break;
+      default:
+        result = {};
+        break;
+    }
     return result;
   };
 
-  // });
   onMounted(async () => {
     await setSeason();
     await searchPlayer();
-    tppRankSquad.value = createdStat('squad');
+    tppRankSolo.value = createdRankStat('solo');
+    tppRankSquad.value = createdRankStat('squad');
+
+    tppSolo.value = createdStat('solo');
+    tppDuo.value = createdStat('tppDuo');
+    tppSquad.value = createdStat('tppSquad');
+
+    if (hasFPP) {
+      fppRankSolo.value = createdRankStat('solo-fpp');
+      fppRankSquad.value = createdRankStat('squad-fpp');
+
+      fppSolo.value = createdStat('solo-fpp');
+      fppDuo.value = createdStat('duo-fpp');
+      fppSquad.value = createdStat('squad-fpp');
+    }
   });
 </script>
 
@@ -73,8 +134,7 @@
     <!-- 3인칭 랭크 솔로, 스쿼드 -->
     <el-row :gutter="20" align="middle" justify="center">
       <el-col :span="10">
-        test
-        <!-- <rank-stat-card mode="solo" tpp :data="testData.squad" /> -->
+        <rank-stat-card solo tpp :data="tppRankSolo" />
       </el-col>
       <el-col :span="10">
         <rank-stat-card squad tpp :data="tppRankSquad" />
@@ -84,9 +144,21 @@
     <!-- 3인칭 일반 솔로, 듀오, 스쿼드 -->
 
     <!-- 1인칭 영역 -->
+    <el-row v-if="hasFPP" :gutter="20" align="middle" justify="center">
+      <el-col :span="10">
+        <rank-stat-card solo fpp :data="fppRankSolo" />
+      </el-col>
+      <el-col :span="10">
+        <rank-stat-card squad fpp :data="fppRankSquad" />
+      </el-col>
+    </el-row>
     <!-- 1인칭 랭크 솔로, 스쿼드 -->
     <!-- 1인칭 일반 솔로, 듀오, 스쿼드 -->
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+  .el-row {
+    margin-bottom: 10px;
+  }
+</style>
