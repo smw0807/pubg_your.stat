f145eaf4de1cc5995d917b5a0101d7b70a0cdad8
Minu Song <smw0807@gmail.com>
2022-11-18 15:41:20 +0900

    테마 변경 컴포넌트...사용 적용은 나중에...

---
+++ src/components/theme/changeTheme.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+  import { useDark, useToggle } from '@vueuse/core';
+  import { Sunny, Moon } from '@element-plus/icons-vue';
+  const isDark = useDark();
+  const toggleDark = useToggle(isDark);
+</script>
+
+<template>
+  <button @click="toggleDark()" text>
+    <el-icon v-if="isDark"><Moon /></el-icon>
+    <el-icon v-else><Sunny /></el-icon>
+  </button>
+</template>
+
+<style></style>
