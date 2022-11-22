eaec24290f4a22d6cb60a27ca82409e5d39db8d4
Minu Song <smw0807@gmail.com>
2022-11-22 19:41:28 +0900

    store action 함수들 리팩터링 프라미스 함수로 변경

--- src/store/search.ts
+++ src/store/search.ts
@@ -29,30 +29,36 @@ export const useSearchStore = defineStore({
   },
   actions: {
     //시즌 정보 세팅
-    async setSeason(param: string): Promise<void> {
-      try {
-        const api = new SeasonAPI(param);
-        const seasons = await api.getSeasons;
+    setSeason(param: string): Promise<void> {
+      return new Promise(async (resolve, reject) => {
+        try {
+          const api = new SeasonAPI(param);
+          const seasons = await api.getSeasons;
 
-        this.nowSeasons = seasons.data.data;
-        this.nowSeason = seasons.data.data.filter(
-          v => v.attributes.isCurrentSeason
-        )[0];
-      } catch (err) {
-        console.error(err);
-      }
+          this.nowSeasons = seasons.data.data;
+          this.nowSeason = seasons.data.data.filter(
+            v => v.attributes.isCurrentSeason
+          )[0];
+          resolve();
+        } catch (err) {
+          reject(err);
+        }
+      });
     },
     //검색
-    async searchPlayer(params: ISearchForm): Promise<void> {
-      try {
-        params.seasonID = this.nowSeason.id;
-        const searchAPI = new PlayersAPI(params);
-        const stat = await searchAPI.allStat;
-        this.rank = stat[0]?.data;
-        this.normal = stat[1]?.data;
-      } catch (err) {
-        console.error(err);
-      }
+    searchPlayer(params: ISearchForm): Promise<void> {
+      return new Promise(async (resolve, reject) => {
+        try {
+          params.seasonID = this.nowSeason.id;
+          const searchAPI = new PlayersAPI(params);
+          const stat = await searchAPI.allStat;
+          this.rank = stat[0]?.data;
+          this.normal = stat[1]?.data;
+          resolve();
+        } catch (err) {
+          reject(err);
+        }
+      });
     },
   },
 });
