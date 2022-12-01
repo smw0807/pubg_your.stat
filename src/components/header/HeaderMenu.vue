931fbed8172ea149bdceb6289032a89aa8d8d9da
Minu Song <smw0807@gmail.com>
2022-12-01 20:34:30 +0900

    메뉴 수정

--- src/components/header/HeaderMenu.vue
+++ src/components/header/HeaderMenu.vue
@@ -11,7 +11,7 @@
       <div class="logo">{{ titleName }}</div>
       <!-- <el-menu-item index="1">Processing Center</el-menu-item> -->
       <div class="flex-grow" />
-      <el-sub-menu index="2">
+      <!-- <el-sub-menu index="2">
         <template #title>Workspace</template>
         <el-menu-item index="2-1">item one</el-menu-item>
         <el-menu-item index="2-2">item two</el-menu-item>
@@ -22,8 +22,8 @@
           <el-menu-item index="2-4-2">item two</el-menu-item>
           <el-menu-item index="2-4-3">item three</el-menu-item>
         </el-sub-menu>
-      </el-sub-menu>
-      <el-menu-item index="3" disabled>Info</el-menu-item>
+      </el-sub-menu> -->
+      <el-menu-item index="0">로그인</el-menu-item>
       <el-menu-item index="4">Orders</el-menu-item>
     </el-menu>
   </div>
