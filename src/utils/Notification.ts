a737570a8955ffe68ecb4498941bdc25ed524a2c
Minu Song <smw0807@gmail.com>
2022-12-17 16:31:59 +0900

    유형별 알림 추가

--- src/utils/Notification.ts
+++ src/utils/Notification.ts
@@ -1,5 +1,8 @@
 /**
  * https://element-plus.org/en-US/component/notification.html
+ *
+ * ## HTML 태그 사용할 수 있는 옵션
+ * dangerouslyUseHTMLString: true
  * 알림 메시지 관련 함수들
  */
 import { ElNotification } from 'element-plus';
@@ -19,5 +22,45 @@ export const _429 = (): void => {
     title: '요청수가 너무 많습니다.',
     message: '잠시후에 다시 검색해주시기 바랍니다.',
     type: 'warning',
+  });
+};
+
+// 성공 알림
+export const notifSuccess = (title: string, msg: string): void => {
+  ElNotification({
+    title: title,
+    message: msg,
+    type: 'success',
+    dangerouslyUseHTMLString: true,
+  });
+};
+
+// 경고 알림
+export const notifWarning = (title: string, msg: string): void => {
+  ElNotification({
+    title: title,
+    message: msg,
+    type: 'warning',
+    dangerouslyUseHTMLString: true,
+  });
+};
+
+// 정보 알림
+export const notifInfo = (title: string, msg: string): void => {
+  ElNotification({
+    title: title,
+    message: msg,
+    type: 'info',
+    dangerouslyUseHTMLString: true,
+  });
+};
+
+// 에러 알림
+export const notifError = (title: string, msg: string): void => {
+  ElNotification({
+    title: title,
+    message: msg,
+    type: 'error',
+    dangerouslyUseHTMLString: true,
   });
 };
