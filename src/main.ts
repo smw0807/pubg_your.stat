b790f07b22b41345a8d450d1fa9f507d706ac425
Minu Song <smw0807@gmail.com>
2022-12-24 16:37:51 +0900

    vue-timeago3 모듈 추가

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,12 @@ import { createApp } from 'vue';
 import App from './App.vue';
 
 import { setupRouter } from '@/routes';
-import { setupElementPlus, setupPinia, setupZoomer } from '@/plugins';
+import { setupElementPlus, setupPinia, setupZoomer, setupTimeago } from '@/plugins';
 
 const app = createApp(App);
 setupRouter(app); //vue-router apply
 setupElementPlus(app); //element plus apply
 setupPinia(app); //pinia apply
 setupZoomer(app); //vue-zoomer apply
+setupTimeago(app);
 app.mount('#app');
