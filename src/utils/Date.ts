1aeacd79bb52d8abd6d0d12ece778f377f97f8de
Minu Song <smw0807@gmail.com>
2023-01-14 14:23:28 +0900

    dayjs로 현재 시간 반환 함수 추가

--- src/utils/Date.ts
+++ src/utils/Date.ts
@@ -1,3 +1,22 @@
+/**
+ * 날짜, 시간 관련 함수들
+ *
+ * dayjs
+ * https://day.js.org/en/
+ * dayjs github
+ * https://github.com/iamkun/dayjs/blob/dev/docs/ko/README-ko.md
+ *
+ * string +format
+ * https://day.js.org/docs/en/parse/string-format
+ *
+ * type
+ * dayjs()만 쓰면 타입은 dayjs.Dayjs
+ * format(template?: string): string
+ */
+import dayjs from 'dayjs';
+import 'dayjs/locale/ko';
+
+//초 데이터 일,시간,분,초로 변환
 export const changeSeconds = (seconds: number): string => {
   if (seconds < 60) return `${seconds}초`;
 
@@ -18,4 +37,9 @@ export const changeSeconds = (seconds: number): string => {
   const d = Math.floor(seconds / (3600 * 24));
   h = Math.floor((seconds % (3600 * 24)) / 3600);
   return `${d}일 ${h}시간 ${m}분 ${s}초`;
+};
+
+//현재시간 포맷 형식으로 반환
+export const nowDateFormat = (format: string): string => {
+  return dayjs().format(format);
 };
