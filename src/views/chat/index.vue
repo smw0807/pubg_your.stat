992b849c0ad16c6b6717b5e63494c1f12217f0cd
Minu Song <smw0807@gmail.com>
2023-02-02 21:55:35 +0900

    팀 채팅 구현 준비....

--- src/views/chat/index.vue
+++ src/views/chat/index.vue
@@ -1,3 +1,15 @@
-<script setup lang="ts"></script>
-<template>gdgd</template>
+<script setup lang="ts">
+  import { useRoute } from 'vue-router';
+
+  const route = useRoute();
+
+  const uid = route.params.uid;
+  //todo 팀 정보 가져오기
+</script>
+<template>
+  <!-- todo 팀 이름, 채팅방 나가기 버튼 -->
+  <!-- todo 접속자 리스트 (왼쪽 사이드) -->
+  <!-- todo 채팅 영역 -->
+  <!-- todo 채팅 입력 영역 -->
+</template>
 <style scoped></style>
