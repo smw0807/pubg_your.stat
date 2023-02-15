78d9717039745a3c71eb8461c41ccd950348af46
Minu Song <smw0807@gmail.com>
2023-02-15 23:42:03 +0900

    IUserPlatformNickNames 인터페이스 주석 추가

--- src/interfaces/Users.ts
+++ src/interfaces/Users.ts
@@ -6,6 +6,6 @@ export interface IUserPlatformNickNames {
   email?: string | null;
   'kakao-nickname': string; //카카오 플레이어 닉네임
   'steam-nickname': string; //스팀 플레이어 닉네임
-  'created-date'?: string;
-  'updated-date'?: string;
+  'created-date'?: string; //최초 등록일
+  'updated-date'?: string; //수정일
 }
