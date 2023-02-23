6a0fd26edad85ee3d7d2552572046bfde844f139
Minu Song <smw0807@gmail.com>
2023-02-23 17:22:12 +0900

    Team api 수정 promise 제거

--- src/apis/firebase/fireStore/Team.ts
+++ src/apis/firebase/fireStore/Team.ts
@@ -11,45 +11,41 @@ export class TeamAPI extends GooleAuthAPI {
 
   // 팀 리스트 가져오기
   async teamList(params: ITeamFilter): Promise<DocumentData> {
-    return new Promise(async (resolve, reject) => {
-      try {
-        let list: DocumentData = [];
-        // 기본 쿼리
-        let q = query(
-          collection(this.db, this.collection),
-          where('platform', '==', params.platform),
-          orderBy('createdAt', params.orderBy)
-        );
-        if (params.isRank !== null) q = query(q, where('isRank', '==', params.isRank));
-        if (params.mode !== 'all') q = query(q, where('mode', '==', params.mode));
+    try {
+      let list: DocumentData = [];
+      // 기본 쿼리
+      let q = query(
+        collection(this.db, this.collection),
+        where('platform', '==', params.platform),
+        orderBy('createdAt', params.orderBy)
+      );
+      if (params.isRank !== null) q = query(q, where('isRank', '==', params.isRank));
+      if (params.mode !== 'all') q = query(q, where('mode', '==', params.mode));
 
-        const querySnapshot = await getDocs(q);
-        list = querySnapshot.docs.map(v => {
-          const rt = v.data();
-          rt.id = v.id;
-          return rt;
-        });
-        resolve(list);
-      } catch (err) {
-        reject(err);
-      }
-    });
+      const querySnapshot = await getDocs(q);
+      list = querySnapshot.docs.map(v => {
+        const rt = v.data();
+        rt.id = v.id;
+        return rt;
+      });
+      return list;
+    } catch (err) {
+      throw err;
+    }
   }
   // 팀 만들기
-  createTeam(params: ITeamInfo): Promise<string> {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const uid = this.nowUser?.uid as string;
-        params.members = [uid];
-        params.roomLeader = uid;
-        params.createdAt = nowDateFormat('YYYY-MM-DD HH:mm:ss');
-        params.maxCount = this.maxCount(params.mode);
-        const docRef = await addDoc(collection(this.db, this.collection), params);
-        resolve(docRef.id);
-      } catch (err) {
-        reject(err);
-      }
-    });
+  async createTeam(params: ITeamInfo): Promise<string> {
+    try {
+      const uid = this.nowUser?.uid as string;
+      params.members = [uid];
+      params.roomLeader = uid;
+      params.createdAt = nowDateFormat('YYYY-MM-DD HH:mm:ss');
+      params.maxCount = this.maxCount(params.mode);
+      const docRef = await addDoc(collection(this.db, this.collection), params);
+      return docRef.id;
+    } catch (err) {
+      throw err;
+    }
   }
 
   //모드에 따라 팀 최대 인원
