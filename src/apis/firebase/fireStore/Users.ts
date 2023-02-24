fb98afe507eaf404cd76db065b60db478447836f
Minu Song <smw0807@gmail.com>
2023-02-24 17:37:56 +0900

    Users api 수정 promise 제거

--- src/apis/firebase/fireStore/Users.ts
+++ src/apis/firebase/fireStore/Users.ts
@@ -11,83 +11,81 @@ export class UsersAPI {
   private message: string = '';
 
   //저장된 플렛폼 플레이 닉네임 가져오기
-  getMyPlatformNickname(uid: string): Promise<IUserPlatformNickNames | null> {
-    return new Promise(async (resolve, reject) => {
-      try {
-        let result: IUserPlatformNickNames | null = null;
-        const docRef = doc(this.db, this.collection, uid);
-        const docSnap = await getDoc(docRef);
-        if (docSnap.exists()) {
-          const data = docSnap.data();
-          result = {
-            email: data.email,
-            'kakao-nickname': data['kakao-nickname'],
-            'steam-nickname': data['steam-nickname'],
-            'created-date': data['created-date'],
-            'updated-date': data['updated-date'],
-          };
-        }
-        resolve(result);
-      } catch (err) {
-        console.error(err);
-        reject(err);
-      }
-    });
+  async getMyPlatformNickname(uid: string): Promise<IUserPlatformNickNames | null> {
+    try {
+      const docRef = doc(this.db, this.collection, uid);
+      const docSnap = await getDoc(docRef);
+      return docSnap.exists()
+        ? {
+            email: docSnap.data().email,
+            'kakao-nickname': docSnap.data()['kakao-nickname'],
+            'steam-nickname': docSnap.data()['steam-nickname'],
+            'created-date': docSnap.data()['created-date'],
+            'updated-date': docSnap.data()['updated-date'],
+          }
+        : null;
+    } catch (err) {
+      console.error(err);
+      throw err;
+    }
   }
 
-  //사용자의 카카오, 스팀 플레이 닉네임 저장
-  savePlatformNickname(
+  //사용자의 카카오, 스팀 플레이 닉네임 저장, 수정
+  async savePlatformNickname(
     mode: 'ins' | 'upd' = 'ins',
     user: User,
     steamNickName: string,
     kakaoNickName: string
   ): Promise<boolean | string> {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const data: IUserPlatformNickNames = {
-          email: user.email,
-          'kakao-nickname': kakaoNickName,
-          'steam-nickname': steamNickName,
-          'updated-date': nowDateFormat('YYYY-MM-DD HH:mm:ss'),
-        };
-        if (mode === 'ins') {
+    try {
+      const data: IUserPlatformNickNames = {
+        email: user.email,
+        'kakao-nickname': kakaoNickName,
+        'steam-nickname': steamNickName,
+        'updated-date': nowDateFormat('YYYY-MM-DD HH:mm:ss'),
+      };
+
+      switch (mode) {
+        case 'ins':
           data['created-date'] = nowDateFormat('YYYY-MM-DD HH:mm:ss');
-        } else {
-          // 기존 created-date 가져오기
+          break;
+        case 'upd':
           const docSnap = await getDoc(doc(this.db, this.collection, user.uid));
           const existingData = docSnap.data();
           data['created-date'] = existingData?.['created-date'] || '';
-        }
-        //플레이어 닉네임 중복체크
-        let isDupl = false;
-        //스팀 중복체크
-        if (steamNickName !== '') {
-          isDupl = await this.duplicateCheck(data.email as string, 'steam', steamNickName);
-        }
-        //카카오 중복체크
-        if (kakaoNickName !== '' && !isDupl) {
-          isDupl = await this.duplicateCheck(data.email as string, 'kakao', kakaoNickName);
-        }
-        //중복 닉네임 있을 시
-        if (isDupl) {
-          resolve(this.message);
-          this.message = '';
-        } else {
-          await setDoc(doc(this.db, this.collection, user.uid), data);
-          resolve(true);
-        }
-      } catch (err) {
-        console.error(err);
-        reject(err);
+          break;
+        default:
+          throw new Error(`Invalid mode: ${mode}`);
       }
-    });
+
+      //플레이어 닉네임 중복체크
+      let isDupl = false;
+      //스팀 중복체크
+      if (steamNickName !== '') {
+        isDupl = await this.duplicateCheck(data.email as string, 'steam', steamNickName);
+      }
+      //카카오 중복체크
+      if (kakaoNickName !== '' && !isDupl) {
+        isDupl = await this.duplicateCheck(data.email as string, 'kakao', kakaoNickName);
+      }
+      //중복 닉네임 있을 시
+      if (isDupl) {
+        return this.message;
+      } else {
+        await setDoc(doc(this.db, this.collection, user.uid), data);
+        return true;
+      }
+    } catch (err) {
+      console.error(err);
+      throw err;
+    }
   }
 
   //사용자 닉네임 중복체크
   async duplicateCheck(email: string, platform: string, nickname: string): Promise<boolean> {
     let result = false;
     try {
-      let q = query(
+      const q = query(
         collection(this.db, this.collection),
         where(`email`, '!=', email),
         where(`${platform}-nickname`, '==', nickname)
@@ -96,10 +94,10 @@ export class UsersAPI {
       if (querySnapshot.docs.length === 0) {
         result = false;
       } else {
-        querySnapshot.docs.map(v => {
-          this.message = '닉네임 [';
-          this.message += v.data()[`${platform}-nickname`];
-          this.message += ']은/는 다른 사용자가 이미 등록한 닉네임입니다.';
+        querySnapshot.docs.forEach(doc => {
+          this.message = `닉네임 [${
+            doc.data()[`${platform}-nickname`]
+          }]은/는 다른 사용자가 이미 등록한 닉네임입니다.`;
         });
         result = true;
       }
@@ -109,6 +107,4 @@ export class UsersAPI {
     }
     return result;
   }
-
-  //todo 플레이 닉네임 수정 함수
 }
