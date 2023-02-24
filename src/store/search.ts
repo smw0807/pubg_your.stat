ffd99157ab8ac6af2075fe494eaccda83be880ab
Minu Song <smw0807@gmail.com>
2023-02-24 17:45:47 +0900

    search store 수정 promise 제거

--- src/store/search.ts
+++ src/store/search.ts
@@ -28,78 +28,68 @@ export const useSearchStore = defineStore({
   },
   actions: {
     //파이어베이스 저장소에 저장된 데이터 가져오기
-    getStats(params: ISearchForm): Promise<number> {
-      return new Promise(async (resolve, reject) => {
-        try {
-          const rs = await statAPI.getStats(params);
-          //저장소에 데이터가 있을 경우
-          if (rs.data()) {
-            this.rank = JSON.parse(rs.data().rank);
-            this.normal = JSON.parse(rs.data().normal);
-            this.lastUpdateDate = rs.data()['last-update-date'];
-          } else {
-            //데이터가 없을 경우 pubg api에다 검색 요청 후 저장소에 저장
-            await this.setSeason(params.platform);
-            await this.searchPlayer(params);
-          }
-          resolve(200);
-        } catch (err) {
-          if (err === 404) player404();
-          if (err === 429) _429();
-          reject(err);
-        }
-      });
-    },
-    //데이터 리로드
-    reloadStats(params: ISearchForm): Promise<void> {
-      return new Promise(async (resolve, reject) => {
-        try {
-          await this.setSeason(params.platform);
-          await this.searchPlayer(params);
-          resolve();
-        } catch (err) {
-          if (err === 404) player404();
-          if (err === 429) _429();
-          reject(err);
-        }
-      });
-    },
-    //시즌 정보 세팅
-    setSeason(param: string): Promise<number> {
-      return new Promise(async (resolve, reject) => {
-        try {
-          const api = new SeasonAPI(param);
-          const seasons = await api.getSeasons;
-
-          this.nowSeasons = seasons.data.data;
-          this.nowSeason = seasons.data.data.filter(v => v.attributes.isCurrentSeason)[0];
-          resolve(200);
-        } catch (err) {
-          reject(errorCode(err));
-        }
-      });
-    },
-    //검색
-    searchPlayer(params: ISearchForm): Promise<number> {
-      return new Promise(async (resolve, reject) => {
-        try {
-          params.seasonID = this.nowSeason.id;
-          const searchAPI = new PlayersAPI(params);
-          const stat = await searchAPI.allStat;
-
-          //파이어베이스에 검색한 스탯정보 저장
-          await statAPI.setStats(params, stat[0].data, stat[1].data);
-          //파이어베이스에 저장된 데이터 가져오기
-          const rs = await statAPI.getStats(params);
-
+    async getStats(params: ISearchForm): Promise<number> {
+      try {
+        const rs = await statAPI.getStats(params);
+        //저장소에 데이터가 있을 경우
+        if (rs.exists()) {
           this.rank = JSON.parse(rs.data().rank);
           this.normal = JSON.parse(rs.data().normal);
           this.lastUpdateDate = rs.data()['last-update-date'];
-          resolve(200);
-        } catch (err) {
-          reject(errorCode(err));
+        } else {
+          //데이터가 없을 경우 pubg api에다 검색 요청 후 저장소에 저장
+          await this.setSeason(params.platform);
+          await this.searchPlayer(params);
         }
-      });
+        return 200;
+      } catch (err) {
+        if (err === 404) player404();
+        if (err === 429) _429();
+        throw err;
+      }
+    },
+    //데이터 리로드
+    async reloadStats(params: ISearchForm): Promise<void> {
+      try {
+        await this.setSeason(params.platform);
+        await this.searchPlayer(params);
+      } catch (err) {
+        if (err === 404) player404();
+        if (err === 429) _429();
+        throw err;
+      }
+    },
+    //시즌 정보 세팅
+    async setSeason(param: string): Promise<number> {
+      try {
+        const api = new SeasonAPI(param);
+        const seasons = await api.getSeasons;
+        this.nowSeasons = seasons.data.data;
+        this.nowSeason = seasons.data.data.filter(v => v.attributes.isCurrentSeason)[0];
+        return 200;
+      } catch (err) {
+        throw errorCode(err);
+      }
+    },
+    //검색
+    async searchPlayer(params: ISearchForm): Promise<number> {
+      try {
+        params.seasonID = this.nowSeason.id;
+        const searchAPI = new PlayersAPI(params);
+        const stat = await searchAPI.allStat;
+
+        //파이어베이스에 검색한 스탯정보 저장
+        await statAPI.setStats(params, stat[0].data, stat[1].data);
+
+        //파이어베이스에 저장된 데이터 가져오기
+        const rs = await statAPI.getStats(params);
+        this.rank = JSON.parse(rs.data().rank);
+        this.normal = JSON.parse(rs.data().normal);
+        this.lastUpdateDate = rs.data()['last-update-date'];
+        return 200;
+      } catch (err) {
+        throw errorCode(err);
+      }
     },
   },
 });
