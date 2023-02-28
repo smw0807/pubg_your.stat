7ddcac92d6a773b9a22a4f4f724c0ab8a06d6ce1
Minu Song <smw0807@gmail.com>
2023-02-28 19:27:04 +0900

    사용자 메세지 컴포넌트 추가 디자인 수정은 해야함 닉네임 위치, 내용이 일정 길이 넘어가면 줄바뀜 되도록 수정 해야함

--- src/components/card/UserMessage.vue
+++ src/components/card/UserMessage.vue
@@ -1,11 +1,50 @@
 <script setup lang="ts">
   import { withDefaults } from 'vue';
-  import { ITeamMessage } from '@/interfaces';
+  import { dateFormat } from '@/utils';
   interface Props {
-    type: 'mine' | 'other' | 'system' | undefined; //메세지 타입
-    data: ITeamMessage | undefined;
+    type: 'mine' | 'other' | undefined; //메세지 타입
+    nickname: string;
+    time: string;
+    message: string;
   }
-  const props = withDefaults(defineProps<Props>(), {});
+  const props = withDefaults(defineProps<Props>(), {
+    type: 'mine',
+    nickname: '',
+    time: '',
+    message: '',
+  });
+  const cardStyles = {
+    float: props.type === 'mine' ? 'right' : 'left',
+  };
+  const cardBodyStyles = {
+    padding: '10px',
+  };
 </script>
-<template></template>
-<style scoped></style>
+<template>
+  <el-card :body-style="cardBodyStyles" :style="cardStyles">
+    <el-row>
+      <el-col :span="18">
+        {{ nickname }}
+      </el-col>
+      <el-col :span="6" align="end">
+        <span :style="`font-size: var(--el-font-size-extra-small)`">
+          {{ dateFormat(time, 'YYYY-MM-DD') }}
+        </span>
+        <br />
+        <span :style="`font-size: var(--el-font-size-small)`">
+          {{ dateFormat(time, 'HH:mm:ss') }}</span
+        >
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="24">
+        {{ message }}
+      </el-col>
+    </el-row>
+  </el-card>
+</template>
+<style scoped>
+  .el-card {
+    width: 30%;
+  }
+</style>
