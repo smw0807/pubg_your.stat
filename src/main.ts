9446d908e2e68f89d50fc31a45f4872417e7dd65
Minu Song <smw0807@gmail.com>
2023-03-16 23:28:01 +0900

    배포 테스트

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import { createApp } from 'vue';
 import App from './App.vue';
 
 import { setupRouter } from '@/routes';
-import { setupElementPlus, setupPinia, setupZoomer, setupTimeago } from '@/plugins';
+import { createPinia } from 'pinia';
+import { setupElementPlus, /*setupPinia,*/ setupZoomer, setupTimeago } from '@/plugins';
 
 const app = createApp(App);
 setupRouter(app); //vue-router apply
 setupElementPlus(app); //element plus apply
-setupPinia(app); //pinia apply
+// setupPinia(app); //pinia apply
+createPinia();
 setupZoomer(app); //vue-zoomer apply
 setupTimeago(app);
 app.mount('#app');
--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,4 +1,4 @@
 export { setupElementPlus } from './ElementPlus';
-export { setupPinia } from './Pinia';
+// export { setupPinia } from './Pinia';
 export { setupTimeago } from './VueTimeago';
 export { setupZoomer } from './Zoomer';
