28ea5d9dba58e393af078ae8c94c1e4867967c3b
Minu Song <smw0807@gmail.com>
2024-01-01 20:03:29 +0900

    fix #10: 새로고침 에러

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,14 +1,17 @@
 import { App } from 'vue';
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router';
 import { setupLayouts } from 'virtual:generated-layouts';
 import generatedRoutes from 'virtual:generated-pages';
 
 const routes = setupLayouts(generatedRoutes);
 
 const router = createRouter({
-  history: createWebHistory(
+  history: createWebHashHistory(
     process.env.NODE_ENV === 'production' ? import.meta.env.VITE_PROJECT_NAME : '/'
   ),
+  // history: createWebHistory(
+  //   process.env.NODE_ENV === 'production' ? import.meta.env.VITE_PROJECT_NAME : '/'
+  // ),
   routes,
   strict: true,
   scrollBehavior(to, from, savedPosition) {
